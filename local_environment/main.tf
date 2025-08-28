24f5b5736c74f413488d6427fba11d9d1fc6d477
Gurgen Yegoryan <gurgen.y@ucraft.com>
2025-08-28 21:24:17 +0400

    add local environment module

---
+++ local_environment/main.tf
@@ -0,0 +1,115 @@
+locals {
+  has_nginx_template       = endswith(var.nginx_template_path, var.none) != true
+  env_file                 = endswith(var.env_file, var.none) != true
+  service_repo             = endswith(var.service_repo, var.none) != true
+  has_valid_suffix         = anytrue([for suffix in var.valid_suffixes_supervisor : endswith(var.proxy_pass, suffix)])
+  has_valid_prefix         = anytrue([for prefix in var.valid_prefixes_supervisor : startswith(var.proxy_pass, prefix)])
+  has_valid_accounts_admin = anytrue([for suffix in var.valid_suffixes_supervisor : endswith(var.proxy_pass_accounts_admin, suffix)])
+}
+
+
+resource "null_resource" "clone_repo" {
+  count = local.service_repo ? 1 : 0
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      if [ ! -d "${var.microservice_catalog_path}/${var.service_repo}" ]; then
+        if [ -d "${var.microservice_catalog_path}/${var.service_repo}" ] && [ -d "${var.microservice_catalog_path}/${var.service_repo}/.git" ]; then
+            echo "Repository ${var.service_repo} already cloned. Skipping..."
+        else
+            git clone --branch ${var.clone_branch} "${var.github_base}/${var.service_repo}" "${var.microservice_catalog_path}/${var.service_repo}"
+        fi
+      fi
+    EOF
+  }
+}
+
+data "template_file" "env" {
+  template = file(var.env_template_path)
+
+  vars = {
+    username          = var.username
+    proxy_pass_proxy  = var.proxy_pass_proxy
+    proxy_pass_public = var.proxy_pass_public
+    protocol          = var.protocol
+    domain            = var.domain
+  }
+}
+
+data "external" "update_env" {
+  program = ["python3", "${path.module}/update_env.py"]
+
+  query = {
+    original_env_content = data.template_file.env.rendered
+    env_vars_to_merge    = jsonencode(var.envs)
+  }
+}
+
+resource "local_file" "env_file" {
+  count      = local.env_file ? 1 : 0
+  content  = data.external.update_env.result["result"]
+  filename = var.env_file
+  depends_on = [resource.null_resource.clone_repo]
+}
+
+
+data "template_file" "supervisor" {
+  template = file(var.supervisor_template_path)
+
+  vars = {
+    service_name = var.service_name
+    service_repo = var.service_repo
+  }
+}
+
+resource "local_file" "supervisor_file" {
+  count      = !local.has_valid_suffix && !local.has_valid_prefix ? 1 : 0
+  content    = data.template_file.supervisor.rendered
+  filename   = var.supervisor_file
+  depends_on = [resource.null_resource.clone_repo]
+
+}
+data "template_file" "nginx" {
+  template = file(var.nginx_template_path)
+
+  vars = {
+    service_name                = var.service_name
+    server_host                 = var.server_host
+    proxy_pass                  = var.proxy_pass
+    proxy_pass_proxy            = var.proxy_pass_proxy
+    proxy_pass_public           = var.proxy_pass_public
+    proxy_pass_accounts_admin   = var.proxy_pass_accounts_admin
+    domain                      = var.domain
+    protocol                    = var.protocol
+    proxy_header_accounts_admin = local.has_valid_accounts_admin ? var.proxy_pass_accounts_admin : var.proxy_pass_default
+    service_repo                = var.service_repo
+    proxy_header                = local.has_valid_suffix ? var.proxy_pass : var.proxy_pass_default
+    locales_path                = var.locales_path
+    styles_path                 = var.styles_path
+  }
+}
+resource "local_file" "nginx_file" {
+  count      = local.has_nginx_template ? 1 : 0
+  content    = data.template_file.nginx.rendered
+  filename   = var.nginx_file
+  depends_on = [resource.null_resource.clone_repo]
+
+}
+data "template_file" "migration_version" {
+  template = file(var.migration_version_template_path)
+
+  vars = {
+    migration_version = var.migration_version
+  }
+}
+
+resource "local_file" "migration_version_file" {
+  count      = var.migration_version_file != null ? 1 : 0
+  content    = data.template_file.migration_version.rendered
+  filename   = var.migration_version_file
+  depends_on = [resource.null_resource.clone_repo]
+}
---
+++ local_environment/variables.tf
@@ -0,0 +1,147 @@
+variable "config_path" {
+  description = "Relative path to the config file"
+  type        = string
+  default     = "config.json"
+}
+variable "env_template_path" {
+  description = "env template path"
+  type        = string
+  default     = null
+}
+variable "env_file" {
+  description = "env template path"
+  type        = string
+  default     = null
+}
+variable "supervisor_template_path" {
+  description = "supervisor template path"
+  type        = string
+  default     = "./terraform/config/templates/supervisord/supervisord.ini"
+}
+variable "migration_version_template_path" {
+  description = "migration-version template path"
+  type        = string
+  default     = "./terraform/config/templates/.migration-version"
+}
+variable "supervisor_file" {
+  description = "supervisor template path"
+  type        = string
+  default     = null
+}
+variable "migration_version_file" {
+  description = "migration template path"
+  type        = string
+  default     = null
+}
+variable "nginx_template_path" {
+  description = "nginx template path"
+  type        = string
+  default     = null
+}
+variable "nginx_file" {
+  description = "nginx template path"
+  type        = string
+  default     = null
+}
+variable "microservice_catalog_path" {
+  description = "Microservice Catalog Folder Path"
+  type        = string
+  default     = "data_volumes/microservice_catalog"
+}
+variable "clone_branch" {
+  description = "Clone Branch"
+  type        = string
+  default     = "master"
+}
+variable "service_repo" {
+  description = "Service Repo"
+  type        = string
+}
+variable "github_base" {
+  description = "GitHub org path"
+  type        = string
+  default     = "git@github.com:MTD-Labs"
+}
+variable "username" {
+  description = "A string of variables to pass to the template"
+  type        = string
+  default     = null
+}
+variable "migration_version" {
+  description = "A string of variables to pass to the template"
+  type        = string
+  default     = null
+}
+variable "service_name" {
+  description = "A string of variables to pass to the template"
+  type        = string
+  default     = null
+}
+variable "server_host" {
+  description = "A string of variables to pass to the template"
+  type        = string
+  default     = "remote"
+}
+variable "proxy_pass_default" {
+  description = "A string of variables to pass to the template"
+  type        = string
+  default     = "$host"
+}
+variable "proxy_pass" {
+  description = "A string of variables to pass to the template"
+  type        = string
+  default     = ""
+}
+variable "proxy_pass_accounts_admin" {
+  description = "A string of variables to pass to the template"
+  type        = string
+  default     = ""
+}
+variable "proxy_pass_public" {
+  description = "A string of variables to pass to the template"
+  type        = string
+  default     = ""
+}
+variable "proxy_pass_proxy" {
+  description = "A string of variables to pass to the template"
+  type        = string
+  default     = ""
+}
+variable "none" {
+  description = "A string of variables to none to the template"
+  type        = string
+  default     = "none"
+}
+variable "domain" {
+  description = "A string of variables to pass to the template"
+  type        = string
+  default     = "trendex.dev"
+}
+variable "protocol" {
+  description = "A string of variables to pass to the template"
+  type        = string
+  default     = "https"
+}
+variable "locales_path" {
+  description = "A string of variables to pass to the template"
+  type        = string
+  default     = "https"
+}
+variable "styles_path" {
+  description = "A string of variables to pass to the template"
+  type        = string
+  default     = "/styles"
+}
+variable "valid_suffixes_supervisor" {
+  description = "List of valid suffixes for server_host"
+  type        = list(string)
+  default     = [".trendex.ai", "trendex.ai/"]
+}
+variable "valid_prefixes_supervisor" {
+  description = "List of valid suffixes for server_host"
+  type        = list(string)
+  default     = ["localhost", "host.docker.internal", "172.16.13", "172.16.12.2:9001"]
+}
+variable "envs" {
+  type = map(string)
+}
