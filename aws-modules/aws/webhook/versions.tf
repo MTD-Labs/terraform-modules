366550fe39be80199dfa4d08cddb84a4aa9f227b
Gurgen Yegoryan <gurgen.y@ucraft.com>
2025-09-12 12:51:01 +0400

    polish verions

--- aws-modules/aws/webhook/versions.tf
+++ aws-modules/aws/webhook/versions.tf
@@ -1,8 +1,6 @@
 terraform {
   required_version = ">= 1.9"
   required_providers {
-    aws     = { source = "hashicorp/aws", version = ">= 5.0" }
     archive = { source = "hashicorp/archive", version = ">= 2.4.0" }
-    random  = { source = "hashicorp/random", version = ">= 3.6.0" }
   }
 }
