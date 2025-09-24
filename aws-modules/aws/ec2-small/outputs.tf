e3181447a378fbb1dd3b615d84ac180167d26657
Gurgen Yegoryan <gurgen.y@ucraft.com>
2025-09-24 13:15:35 +0400

    fix

--- aws-modules/aws/ec2-small/outputs.tf
+++ aws-modules/aws/ec2-small/outputs.tf
@@ -17,11 +17,3 @@ output "instance_id" {
   description = "Instance ID"
   value       = aws_instance.bastion.id # Use the splat operator to get a list of IDs
 }
-
-output "cloudflare_stage_records" {
-  value = {
-    stage     = try(cloudflare_record.stage_a[0].hostname, null)
-    wildcard  = try(cloudflare_record.stage_wildcard_a[0].hostname, null)
-    target_ip = local.target_public_ip
-  }
-}
