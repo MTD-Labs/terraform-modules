23d72b80f26b7b23abc80283dade31aba8b6deff
Gurgen Yegoryan <gurgen.y@ucraft.com>
2025-09-24 12:03:07 +0400

    fix

--- aws-modules/presets/small/versions.tf
+++ aws-modules/presets/small/versions.tf
@@ -9,14 +9,11 @@ terraform {
       configuration_aliases = [aws.main, aws.us_east_1]
     }
 
-    local    = ">= 2.2.2"
-    null     = ">= 3.1.1"
-    template = ">= 2.2.0"
-    random   = ">= 3.4.3"
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 4.43"
-    }
+    local      = ">= 2.2.2"
+    null       = ">= 3.1.1"
+    template   = ">= 2.2.0"
+    random     = ">= 3.4.3"
+    cloudflare = "~> 4.43"
   }
 }
 
