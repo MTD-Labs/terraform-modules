288d3d1ccbecb6b1f31b295cd3f6e45ef96ce8da
Gurgen Yegoryan <gurgen.y@ucraft.com>
2025-10-16 01:13:31 +0400

    fix descriptopm

--- aws-modules/aws/ecs/loki.tf
+++ aws-modules/aws/ecs/loki.tf
@@ -72,7 +72,7 @@ resource "aws_security_group_rule" "grafana_from_alb" {
   protocol                 = "tcp"
   security_group_id        = aws_security_group.loki_grafana[0].id
   source_security_group_id = var.alb_security_group_id
-  description              = "ALB -> Grafana 3000"
+  description              = "ALB Grafana 3000"
 }
 
 
