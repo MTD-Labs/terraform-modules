b72f0c2221e30102509795624ef87166a29420c3
Gurgen Yegoryan <gurgen.y@ucraft.com>
2025-10-20 21:12:27 +0400

    fix

--- aws-modules/aws/ingress-controller/main.tf
+++ aws-modules/aws/ingress-controller/main.tf
@@ -1,3 +1,13 @@
+provider "kubernetes" {
+  host                   = var.cluster_endpoint
+  cluster_ca_certificate = base64decode(var.cluster_ca_cert)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command     = "aws"
+  }
+}
+
 data "archive_file" "nginx_controller_values" {
   type        = "zip"
   source_file = "${var.values_file_path}/values-${var.env}.yaml"
