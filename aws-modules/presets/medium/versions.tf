c7a2017459999171ad69dae3140661b58a5b27ac
Gurgen Yegoryan <gurgen.y@ucraft.com>
2025-10-23 21:42:51 +0400

    fix

--- aws-modules/presets/medium/versions.tf
+++ aws-modules/presets/medium/versions.tf
@@ -8,14 +8,6 @@ terraform {
       version               = ">= 5.25.0"
       configuration_aliases = [aws.main, aws.us_east_1]
     }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      configuration_aliases = [kubernetes.eks]
-    }
-    helm = {
-      source = "hashicorp/helm"
-      configuration_aliases = [helm.eks]
-    }
     local    = ">= 2.2.2"
     null     = ">= 3.1.1"
     template = ">= 2.2.0"
