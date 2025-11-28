1a150c29ee7ec34f363e1100b10c370b0affc947
Gurgen Yegoryan <gurgen.y@ucraft.com>
2025-11-28 22:58:49 +0400

    update ports

--- aws-modules/aws/ec2-small/sg.tf
+++ aws-modules/aws/ec2-small/sg.tf
@@ -3,40 +3,33 @@ resource "aws_security_group" "sg" {
   description = "Security group for ${local.name}."
   vpc_id      = var.vpc_id
 
-  # Allow HTTP (port 80) traffic
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_cidr_blocks
+  # Dynamically allow all ports defined in allowed_tcp_ports
+  dynamic "ingress" {
+    for_each = var.allowed_tcp_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 
-  # Allow HTTPS (port 443) traffic
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_cidr_blocks
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_cidr_blocks
-  }
+  # Additional static ingress rule: 3000
   ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = var.allowed_cidr_blocks
   }
+
+  # Additional static ingress rule: 8000 (open to all)
   ingress {
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   # Allow all outbound traffic
   egress {
     from_port   = 0
