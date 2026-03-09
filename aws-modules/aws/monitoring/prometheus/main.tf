381814743b2af2265cbc2b353b1a3d37244dc4a7
Gurgen Yegoryan <gurgen.y@ucraft.com>
2026-03-09 19:29:05 +0400

    feat: add helm_release for prometheus_crds and update dependencies

--- aws-modules/aws/monitoring/prometheus/main.tf
+++ aws-modules/aws/monitoring/prometheus/main.tf
@@ -5,6 +5,34 @@ data "archive_file" "prometheus_values" {
   output_path = "/tmp/prometheus_helm_dir_checksum.zip"
 }
 
+resource "helm_release" "prometheus_crds" {
+  name       = "prometheus-crds"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  version    = "79.5.0"
+
+  namespace  = "monitoring"
+  create_namespace = true
+
+  wait = true
+
+  set = [
+    {
+      name  = "prometheus.enabled"
+      value = "false"
+    },
+    {
+      name  = "alertmanager.enabled"
+      value = "false"
+    },
+
+    {
+      name  = "grafana.enabled"
+      value = "false"
+    }
+  ]
+}
+
 resource "helm_release" "prometheus" {
   count            = var.eks_enabled == true ? 1 : 0
   name             = "prometheus"
@@ -13,12 +41,16 @@ resource "helm_release" "prometheus" {
   version          = "79.5.0"
   namespace        = "monitoring"
   create_namespace = true
+  wait    = true
+  timeout = 600
 
   values = [templatefile("${var.values_file_path}/values-${var.env}.yaml", {
     subnets = join(",", var.subnets)
   })]
 
-  depends_on = [data.archive_file.prometheus_values]
+  depends_on = [
+    helm_release.prometheus_crds
+  ]
 
   # lifecycle {
   #   ignore_changes = [
